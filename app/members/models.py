3c757090adbff06c4c3f4feeb59b2578621c3ef1
teachmesomething2580 <teachmesomething2580@gmail.com>
2018-11-14 00:54:30 -0800

    User 객체에 object를 MyUserManager와 연결

--- app/members/models.py
+++ app/members/models.py
@@ -2,26 +2,36 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 
-from cashes.models import Hammer
+from members.manager import MyUserManager
 
 
 class User(AbstractUser):
     phone = PhoneNumberField(unique=True)
     email = models.EmailField(unique=True)
     name = models.CharField(max_length=10)
-    sns_agree = models.BooleanField(blank=True, default=False)
-    email_agree = models.BooleanField(blank=True, default=False)
+    sns_agree = models.BooleanField(default=False)
+    email_agree = models.BooleanField(default=False)
     online_available_use_category_limit = models.CharField(
         choices='',
         max_length=4,
+        blank=True,
     )
     rating = models.CharField(
         choices='',
         max_length=4,
+        blank=True,
     )
     hammer = models.PositiveIntegerField(default=0)
     happy_cash = models.PositiveIntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    objects = MyUserManager()
+
     def __str__(self):
         return self.username
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+
+
