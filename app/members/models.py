56d6c1a027eca5a8cbf052da55f73e3349bdade9
teachmesomething2580 <teachmesomething2580@gmail.com>
2018-11-13 22:30:02 -0800

    members 필드 추가

--- app/members/models.py
+++ app/members/models.py
@@ -1,8 +1,24 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 class User(AbstractUser):
-    img_profile = models.ImageField(
-        upload_to='user'
+    name = models.CharField(
+        max_length=10,
     )
+    phone = PhoneNumberField()
+    email = models.EmailField()
+    sns_agree = models.BooleanField(blank=True, default=False)
+    email_agree = models.BooleanField(blank=True, default=False)
+    online_available_use_category_limit = models.CharField(
+        choices='',
+        max_length=4,
+    )
+    rating = models.CharField(
+        choices='',
+        max_length=4,
+    )
+    hammer = models.PositiveIntegerField(default=0)
+    happy_cash = models.PositiveIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
