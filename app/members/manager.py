5d209e974f29cc27dcf745fdd5a5271d7970b7a6
teachmesomething2580 <teachmesomething2580@gmail.com>
2018-11-14 01:04:29 -0800

    manager 주석 추가

--- app/members/manager.py
+++ app/members/manager.py
@@ -6,5 +6,12 @@ from cashes.models import Hammer
 class MyUserManager(UserManager):
     def create_user(self, username, email=None, password=None, **extra_fields):
         user = super().create_superuser(username, email, password, **extra_fields)
-        Hammer.give_hammer_point(user, 500)
+
+        # 회원가입시 500 Hammer 증정
+        Hammer.give_hammer_point(
+            content='회원가입 500원',
+            amount=500,
+            use_or_save='s',
+            user_id=user,
+        )
         return user
