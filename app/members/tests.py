0b6b80f0e61cc86f186d172bc101b13913d37610
teachmesomething2580 <teachmesomething2580@gmail.com>
2018-11-14 00:54:57 -0800

    유저 생성시 Hammer 500을 추가하는 Test 코드 추가

--- app/members/tests.py
+++ app/members/tests.py
@@ -5,12 +5,27 @@ User = get_user_model()
 
 
 class UserModelTest(TestCase):
+    # 유저 생성
     def setUp(self):
         user = User.objects.create_user(
             username='example1',
             password='asd',
+            phone='+8201055555555',
+            email='ex@ex1.com',
+            name="KingGod"
         )
         self.user = user
 
+    # __str__ 확인
     def test_string_representation(self):
         self.assertEqual(str(self.user), 'example1')
+
+    # 회원가입시 500 해머 확인
+    def test_first_register_500_point(self):
+        self.assertEqual(self.user.hammer, 500)
+
+
+class UserHammerRelationTest(TestCase):
+    def setUp(self):
+        user = User.objects.get(pk=1)
+        self.user = user
