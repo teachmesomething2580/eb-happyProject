b2f3788e7eec956c7e514808bff46282aeea33ee
teachmesomething2580 <teachmesomething2580@gmail.com>
2018-12-04 00:03:51 -0800

    입점몰 Serializer 작성

--- app/use_point/apis/serializers.py
+++ app/use_point/apis/serializers.py
@@ -10,11 +10,25 @@ class UsePointSerializer(serializers.ModelSerializer):
         depth = 1
 
 
+class UsePointImportListSerializer(serializers.ListSerializer):
+    def to_representation(self, data):
+        data = data.filter(is_online=True, where_to_use__is_import_point=True)
+        return super(UsePointImportListSerializer, self).to_representation(data)
+
+
+class UsePointImportSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UsePoint
+        fields = '__all__'
+        list_serializer_class = UsePointImportListSerializer
+
+
 class CategoryUsePointSerializer(serializers.ModelSerializer):
+    usepoint_set = UsePointImportSerializer(read_only=True, many=True)
+
     class Meta:
         model = UsePointCategory
         fields = (
             'name',
             'usepoint_set',
         )
-        depth = 1
