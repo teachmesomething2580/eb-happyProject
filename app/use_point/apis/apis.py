a5d775898497b29e60d806a9a45e79a1034eeab6
teachmesomething2580 <teachmesomething2580@gmail.com>
2018-12-07 03:28:41 -0800

    usepoint, event Pagination 구현

--- app/use_point/apis/apis.py
+++ app/use_point/apis/apis.py
@@ -13,6 +13,9 @@ from use_point.models import UsePoint, UsePointCategory
 
 
 class UsePointListGenericAPIView(generics.ListAPIView):
+    """
+    UsePoint List를 전부 내려주는 API
+    """
     __basic_fields = ('name', 'is_online', 'category__name', )
     queryset = UsePoint.objects.all()
     serializer_class = UsePointSerializer
@@ -31,12 +34,34 @@ class UsePointListGenericAPIView(generics.ListAPIView):
         return context
 
 
+class UsePointRetrieveGenericAPIView(generics.RetrieveAPIView):
+    """
+    특정 UsePoint API를 내려주는 API
+    """
+    queryset = UsePoint.objects.all()
+    serializer_class = UsePointSerializer
+
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        if self.request.user.is_authenticated:
+            context.update({
+                'request': self.request
+            })
+        return context
+
+
 class CategoryUsePointListGenericAPIView(generics.ListAPIView):
+    """
+    Category별로 UsePoint를 내려주는 API
+    """
     queryset = UsePointCategory.objects.all()
     serializer_class = CategoryUsePointSerializer
 
 
 class CategoryListGenericAPIView(generics.ListAPIView):
+    """
+    Category List를 내려주는 API
+    """
     __basic_fields = ('name', 'usepoint__is_online', )
     queryset = UsePointCategory.objects.all().distinct()
     serializer_class = CategorySerializer
@@ -46,6 +71,9 @@ class CategoryListGenericAPIView(generics.ListAPIView):
 
 
 class UsePointLikeGenericAPIView(APIView):
+    """
+    UsePoint 좋아요를 설정하는 API
+    """
     permission_classes = (
         permissions.IsAuthenticated,
     )
--- app/use_point/apis/pagination.py
+++ app/use_point/apis/pagination.py
@@ -1,6 +1,32 @@
-from rest_framework.pagination import PageNumberPagination
+from collections import OrderedDict
+
+from rest_framework.pagination import PageNumberPagination, _get_displayed_page_numbers, _get_page_links
+from rest_framework.response import Response
+from rest_framework.utils.urls import remove_query_param, replace_query_param
 
 
 class UsePointResultSetPagination(PageNumberPagination):
     page_size = 51
     page_size_query_param = 'page_size'
+
+    def get_paginated_response(self, data):
+        base_url = self.request.build_absolute_uri()
+
+        def page_number_to_url(page_number):
+            if page_number == 1:
+                return remove_query_param(base_url, self.page_query_param)
+            else:
+                return replace_query_param(base_url, self.page_query_param, page_number)
+
+        current = self.page.number
+        final = self.page.paginator.num_pages
+        page_numbers = _get_displayed_page_numbers(current, final)
+        page_links = _get_page_links(page_numbers, current, page_number_to_url)
+
+        return Response(OrderedDict([
+            ('count', self.page.paginator.count),
+            ('next', self.get_next_link()),
+            ('previous', self.get_previous_link()),
+            ('page_links', page_links),
+            ('results', data),
+        ]))
